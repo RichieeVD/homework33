238fc445e3a576f7c9953fbe4b9e2bbcf3e70d50
rvd <nseries_nokia@list.ru>
2026-02-24 20:37:07 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,27 @@ public class Main {
         int year = 2026;
         printIsLeapYear(year);
     }
-    public static void printIsLeapYear(int year) {
 
+    public static void printIsLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год — високосный год");
         } else {
             System.out.println(year + " год — невисокосный год");
         }
+    }
 
+    // Задача 2
+    public static void recommendApplication(int osType, int deviceYear) {
+        int currentYear = 2021;
+
+        if (osType == 0 && deviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (osType == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (osType == 1 && deviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (osType == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
